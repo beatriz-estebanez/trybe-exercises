dd9c963d6530e8db61282b7ab062cc229d115a5a
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-02-25 22:10:00 -0300

    Programa 6 - xadrez

--- Fundamentos_Desenv_Web/Bloco_4/dia_1/script6.js
+++ Fundamentos_Desenv_Web/Bloco_4/dia_1/script6.js
@@ -1,13 +1,17 @@
-let a = 2;
-let b = 4;
+let chess_piece = "QUEEN";
 
-console.log("Adição:")
-console.log(a+b)
-console.log("Subtração:")
-console.log(a-b)
-console.log("Multiplicação:")
-console.log(a*b)
-console.log("Divisão:")
-console.log(a/b)
-console.log("Módulo:")
-console.log(a%b)
+if (chess_piece.toLowerCase() === "king") {
+  console.log("casa em casa na horizontal, vertical e diagonal")
+} else if (chess_piece.toLowerCase() === "queen") {
+  console.log("livre movimentação na horizontal, vertical e diagonal");
+} else if (chess_piece.toLowerCase() === "bishop") {
+  console.log("livre movimentação na diagonal");
+} else if (chess_piece.toLowerCase() === "rook") {
+  console.log("livre movimentação na horizontal e na vertical");
+} else if (chess_piece.toLowerCase() === "knight") {
+  console.log("duas casas em L");
+} else if (chess_piece.toLowerCase() === "pawn"){
+  console.log("movimentos frontais, ataque na diagonal");
+} else {
+  console.log("Peça inválida")
+}
