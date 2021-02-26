48564cfbb5623c8d834e8dc79351dbccdcaf150e
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-02-26 08:39:07 -0300

    Programa 7 - conversao de notas em conceitos

--- Fundamentos_Desenv_Web/Bloco_4/dia_1/script7.js
+++ Fundamentos_Desenv_Web/Bloco_4/dia_1/script7.js
@@ -1,13 +1,17 @@
-let a = 2;
-let b = 4;
+let nota = 56;
 
-console.log("Adição:")
-console.log(a+b)
-console.log("Subtração:")
-console.log(a-b)
-console.log("Multiplicação:")
-console.log(a*b)
-console.log("Divisão:")
-console.log(a/b)
-console.log("Módulo:")
-console.log(a%b)
+if (nota < 0 || nota > 100) {
+  console.log("Nota inválida!");
+} else if (nota >= 90) {
+  console.log("A");
+} else if (nota >= 80) {
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if (nota >= 60) {
+  console.log("D");
+} else if (nota >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}
