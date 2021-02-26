3a6117366237c340cf2cec05ea96260cde9316bd
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-02-25 21:45:16 -0300

    Programa 5 - triangulo

--- Fundamentos_Desenv_Web/Bloco_4/dia_1/script5.js
+++ Fundamentos_Desenv_Web/Bloco_4/dia_1/script5.js
@@ -1,13 +1,10 @@
-let a = 2;
-let b = 4;
+let ang1 = 90, ang2 = 80, ang3= 10;
 
-console.log("Adição:")
-console.log(a+b)
-console.log("Subtração:")
-console.log(a-b)
-console.log("Multiplicação:")
-console.log(a*b)
-console.log("Divisão:")
-console.log(a/b)
-console.log("Módulo:")
-console.log(a%b)
+
+if (ang1 <= 0 || ang2 <= 0 || ang3 <= 0) {
+  console.log("Ângulo inválido!")
+} else if (ang1 + ang2 + ang3 == 180) {
+  console.log(true);
+} else {
+  console.log(false);
+}
