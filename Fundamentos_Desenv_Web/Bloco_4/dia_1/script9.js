acd50b07d5bbcc01a0a527663413db4ec46f023c
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-02-26 08:57:37 -0300

    Programa 9 - numero impar

---
+++ Fundamentos_Desenv_Web/Bloco_4/dia_1/script9.js
@@ -0,0 +1,11 @@
+// Verifica se pelo menos um dos três números é ímpar
+
+let number1 = 52;
+let number2 = 12;
+let number3 = 23;
+
+if (number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !==0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
