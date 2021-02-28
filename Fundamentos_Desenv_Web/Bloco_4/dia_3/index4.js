83ebb80bf476258455ef9028f5733ecd64bdd5ca
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-02-28 14:55:30 -0300

    Exericio 4 - alteraçao na variavel numLinhas

--- Fundamentos_Desenv_Web/Bloco_4/dia_3/index4.js
+++ Fundamentos_Desenv_Web/Bloco_4/dia_3/index4.js
@@ -2,15 +2,16 @@
 
 // O número deve ser ímpar e maior que 1 para formar uma pirâmide
 
-let n = 29;
+let n = 7;
 
 if (n % 2 === 0 || n <= 1) {
   console.log("O número informado deve ser ímpar e maior que 1");
 } else {
 
   let numAsteriscos = 1;
+  let numLinhas = n;
 
-  for (let lin = 1; lin <= n; lin += 1) {
+  for (let lin = 1; lin <= numLinhas; lin += 1) {
     let line = '';
     let ref = (n - numAsteriscos) / 2;
     for (let col = 1; col <= n; col += 1) {
@@ -21,9 +22,7 @@ if (n % 2 === 0 || n <= 1) {
       }
     }
     numAsteriscos += 2;
+    numLinhas -= 1;
     console.log(line);
-    if (ref === 0) {
-      break
-    }
   }
 }
