bcb6f9bae781d6e62378df4a770cec3a53aaace1
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-01 20:20:00 -0300

    Objetos e For in - exercicios 3 e 4

--- Fundamentos_Desenv_Web/Bloco_4/dia_4/script1.js
+++ Fundamentos_Desenv_Web/Bloco_4/dia_4/script1.js
@@ -5,11 +5,18 @@ let info = {
 };
 
 // 1. Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-
 console.log('Bem vinda, ' + info.personagem);
 
 // 2. Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
-
 info['recorrente'] = 'Sim';
 console.log(info);
 
+// 3. Faça um for/in que mostre todas as chaves do objeto.
+for (let key in info) {
+  console.log(key);
+}
+
+// 4. Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+for (let key in info) {
+  console.log(info[key]);
+}
