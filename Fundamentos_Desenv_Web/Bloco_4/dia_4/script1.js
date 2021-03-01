5aa64e2fa1640f3391407dd6e6def5c7874d93cd
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-01 20:37:08 -0300

    Objetos e For in - exercicio 5

--- Fundamentos_Desenv_Web/Bloco_4/dia_4/script1.js
+++ Fundamentos_Desenv_Web/Bloco_4/dia_4/script1.js
@@ -20,3 +20,24 @@ for (let key in info) {
 for (let key in info) {
   console.log(info[key]);
 }
+
+// 5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Não",
+};
+
+console.log('   ')
+
+for (let key in info) {
+  if (info[key] === info.recorrente) {
+    if (info.recorrente === "Sim" && info2.recorrente === "Sim") {
+      console.log('Ambos recorrentes');
+    } else {
+      console.log(info[key] + ' e ' + info2[key]);
+    }
+  }
+}
