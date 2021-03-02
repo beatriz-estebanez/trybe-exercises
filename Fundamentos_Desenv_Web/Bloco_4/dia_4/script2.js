415fd65c6ab51c302191e5adea7d56a9b45f7eb4
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-02 15:58:16 -0300

    Exericio 2 - funçao maior valor

--- Fundamentos_Desenv_Web/Bloco_4/dia_4/script2.js
+++ Fundamentos_Desenv_Web/Bloco_4/dia_4/script2.js
@@ -15,3 +15,19 @@ function verificaPalindrome (word) {
 }
 
 console.log(verificaPalindrome ('arara'));
+
+// 2. Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function indiceMaiorValor (array) {
+  let indiceMaior = 0;
+  for (let index in array) {
+    if (array[index] > array[indiceMaior]) {
+      indiceMaior = index;
+    }
+  }
+  return 'O índice de maior valor é ' + indiceMaior;
+} 
+
+
+let arrayTeste = [2, 3, 6, 7, 10, 1];
+console.log(indiceMaiorValor(arrayTeste));
