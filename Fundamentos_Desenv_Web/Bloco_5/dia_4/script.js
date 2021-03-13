e4831942c2d8298483b2188d9bd40ce9883be299
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-13 10:04:01 -0300

    altera a cor do texto

--- Fundamentos_Desenv_Web/Bloco_5/dia_4/script.js
+++ Fundamentos_Desenv_Web/Bloco_5/dia_4/script.js
@@ -1,9 +1,18 @@
-let changeColor = document.querySelectorAll('#cor-fundo li');
+let changeBackgroundColor = document.querySelectorAll('#cor-fundo li');
+let changeFontColor = document.querySelectorAll('#cor-texto li');
 
-for (let index = 0; index < changeColor.length; index += 1) {
-  changeColor[index].addEventListener('click', function () {
-    let newColor = changeColor[index].id;
+for (let index = 0; index < changeBackgroundColor.length; index += 1) {
+  changeBackgroundColor[index].addEventListener('click', function () {
+    let newColor = changeBackgroundColor[index].id;
     console.log(newColor);
     document.querySelector('body').style.backgroundColor = newColor;
+  })
+}
+
+for (let index2 = 0; index2 < changeFontColor.length; index2 += 1) {
+  changeFontColor[index2].addEventListener('click', function () {
+    let newFontColor = changeFontColor[index2].id;
+    console.log(newFontColor);
+    document.querySelector('body').style.color = newFontColor;
   })
 }
