a00f3a006f43b6967c41d4315e0106e46ccdad38
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-03-26 18:14:37 -0300

    Exercicio 1 parte 1

---
+++ Fundamentos_Desenv_Web/Bloco_7/Dia_3/parte1/script1.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(sum(4,5), 9, 'Erro: 4 + 5 = 9');
+assert.strictEqual(sum(0,0), 0, 'Erro: resultado deveria ser zero');
+const soma = sum(4,'5');
