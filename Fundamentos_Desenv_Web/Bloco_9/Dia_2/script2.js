9a2f21d5c248fec04ed7a1f96d7b48eb72e73fff
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-04-13 19:00:24 -0300

    Exercicio 3 e 4 - using promises

--- Fundamentos_Desenv_Web/Bloco_9/Dia_2/script2.js
+++ Fundamentos_Desenv_Web/Bloco_9/Dia_2/script2.js
@@ -1,6 +1,10 @@
 const { rejects } = require("assert");
 const { resolve } = require("path");
 
+const resultDivision = (sum) => {
+  return [2, 3, 5, 10].map((number) => sum / number);
+} // ideia retirada do gabarito do course
+
 const checkSumArray = () => {
 
   const array = [];
@@ -8,19 +12,19 @@ const checkSumArray = () => {
   for (let index = 0; index < 10; index += 1) {
     array.push(Math.pow(Math.floor(Math.random() * 50), 2));
   }
-  
+
   const soma = array.reduce((acc, crr) => acc + crr, 0);
   console.log(soma);
 
   const promise = new Promise((resolve, reject) => {
     if (soma < 8000) {
-      resolve('Promise resolvida');
+      resolve(soma);
     } else {
-      reject('Promise rejeitada');
+      reject('É mais de oito mil! Essa promise deve estar quebrada!');
     }
   })
-  .then(resp => console.log(resp))
-  .catch(resp => console.log(resp));
+  .then(sum => console.log(resultDivision(sum)))
+  .catch(error => console.log(error));
 }
 
 checkSumArray();
