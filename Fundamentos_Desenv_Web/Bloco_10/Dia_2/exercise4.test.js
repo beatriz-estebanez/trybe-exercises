d9c5e5fb9a7b0069d66bb5ec1d876ef678f86834
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-04-22 08:18:14 -0300

    Exercicio 4 - using jest async

---
+++ Fundamentos_Desenv_Web/Bloco_10/Dia_2/exercise4.test.js
@@ -0,0 +1,30 @@
+// busca repositótios por URL
+// retorna LISTA
+
+const fetch = require('node-fetch');
+
+const getRepos = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      return data.map((repo) => repo.name);
+    });
+};
+
+/* const teste = async () => {
+  const result = await getRepos('https://api.github.com/orgs/tryber/repos');
+  console.log(result);
+}
+
+teste(); */
+
+
+describe('testing getRepos function', () => {
+  it('return that two repos are in the list', () => {
+    expect.assertions(2);
+    return getRepos('https://api.github.com/orgs/tryber/repos').then(listNames => {
+      expect(listNames).toContain('sd-01-week4-5-project-todo-list');
+      expect(listNames).toContain('sd-01-week4-5-project-meme-generator');
+    });
+  });
+});
